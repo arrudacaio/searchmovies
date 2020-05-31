a5973964cfc0b03b85d3ee335b3a32f4bfbab23c
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-05-31 03:20:48 -0300

    created card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './styles.css';
 
-function Card(props){
-
+function Card(){
+    return(
+        <div className="card">
+            <img src="https://upload.wikimedia.org/wikipedia/pt/3/3a/Interstellar_Filme.png" className="card-img" />
+            <div className="card-info">
+                <h1 className="card-info__title">Titulo do filme</h1>
+                <h2 className="card-info__data">November 5, 2014</h2>
+                <p className="card-info__resume">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import Card from "../../components/Card/Card";
+import "./styles.css";
 
-function Main(){
-    return(
-        <div className="container">
-            <h1 className="container-title">SEARCH  MOVIES</h1>
-            <input type="text" placeholder="Movie Name..." className="container-input"/>
-        </div>
-    );
+function Main() {
+  return (
+    <div className="box">
+      <div className="container">
+        <h1 className="container-title">SEARCH MOVIES</h1>
+
+        <input
+          type="text"
+          placeholder="Movie Name..."
+          className="container-input"
+        />
+        <select>
+          <option>Sort by</option>
+          <option>Value 2</option>
+        </select>
+      </div>
+      <Card />
+    </div>
+  );
 }
 
 export default Main;
