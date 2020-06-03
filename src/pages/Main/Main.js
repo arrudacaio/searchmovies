a18a9da3d6546e21c0328f12b5256e0d763c4bc6
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-06-03 04:59:18 -0300

    api connection

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "../../components/Card/Card";
-import api from "../../services/api";
+import axios from "axios";
 import "./styles.css";
 
 function Main() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {},
+  });
+
+  const apiurl = "https://api.themoviedb.org/3/search/movie?api_key=172d2bcd975eca6acc08c0a239f0dc85&language=en-US";
+
+  const handleInput = e => {
+    const s = e.target.value;
+
+    setState(prevState => {
+      return {...prevState, s: s}
+    });
+
+  }
+
+  const search = async e => {
+    if(e.key === "Enter") {
+      await axios(apiurl + "&query=" + state.s).then(({data}) => {
+        let results = data.results;
+        
+        setState(prevState => {
+          return {...prevState, results: results}
+        })
+      })
+    }
+  }
+
+
   return (
     <div className="box">
       <div className="container">
         <h1 className="container-title">SEARCH MOVIES</h1>
-        <form>
-          <input
-            type="text"
-            placeholder="Movie Name..."
-            className="container-input"
-          />
-          <button type="submit">SEARCH</button>
-        </form>
+        <input
+          type="text"
+          placeholder="Movie Name..."
+          className="container-input"
+          onChange={handleInput}
+          onKeyPress={search}
+        />
       </div>
       <Card
         title="interestelar"
