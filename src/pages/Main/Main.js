bedce14c8c1838c92ed37ec21f3bee2621c74528
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-06-03 23:55:15 -0300

    finish app

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Card from "../../components/Card/Card";
 import axios from "axios";
 import "./styles.css";
@@ -8,41 +8,47 @@ function Main() {
     s: "",
     results: [],
     selected: {},
+    adult: "",
   });
 
-  const apiurl = "https://api.themoviedb.org/3/search/movie?api_key=172d2bcd975eca6acc08c0a239f0dc85&language=en-US";
+  const apiurl =
+    "https://api.themoviedb.org/3/search/movie?api_key=172d2bcd975eca6acc08c0a239f0dc85&language=en-US";
 
   const baseImg = "https://image.tmdb.org/t/p/w500";
 
-
-  const handleInput = e => {
+  const handleInput = (e) => {
     const s = e.target.value;
 
-    setState(prevState => {
-      return {...prevState, s: s}
+    setState((prevState) => {
+      return { ...prevState, s: s };
+    });
+  };
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      await axios(apiurl + "&query=" + state.s).then(({ data }) => {
+        let results = data.results;
+
+        setState((prevState) => {
+          return { ...prevState, results: results };
+        });
+      });
+    }
+  };
+
+  const handleAdult = (e) => {
+    setState((prevState) => {
+      return { ...prevState, adult: e };
     });
 
-  }
-
-  const search = async e => {
-    if(e.key === "Enter") {
-      await axios(apiurl + "&query=" + state.s).then(({data}) => {
-        let results = data.results;
-        
-        setState(prevState => {
-          return {...prevState, results: results}
-        })
-      })
-    }
-  }
-
-
-
+    console.log(e.target.value);
+  };
 
   return (
     <div className="box">
       <div className="container">
         <h1 className="container-title">SEARCH MOVIES</h1>
+
         <input
           type="text"
           placeholder="Movie Name..."
@@ -50,15 +56,33 @@ function Main() {
           onChange={handleInput}
           onKeyPress={search}
         />
+        <select value={state.adult} onChange={handleAdult}>
+          <option value="">Included Adult Movies?</option>
+          <option value="yes">Yes</option>
+          <option value="dont">Dont</option>
+        </select>
       </div>
-      {state.results.map(result => (
-        <Card 
-          imgURL={baseImg + result.poster_path}
-          title={result.title}
-          data={result.release_date}
-          resume={result.overview}
-        />
-      ))}
+      {state.adult === "yes"
+        ? state.results
+            .filter((movie) => movie.adult === true)
+            .map((result) => (
+              <Card
+                imgURL={baseImg + result.poster_path}
+                title={result.title}
+                data={result.release_date}
+                resume={result.overview}
+              />
+            ))
+        : state.results
+            .filter((movie) => movie.adult === false)
+            .map((result) => (
+              <Card
+                imgURL={baseImg + result.poster_path}
+                title={result.title}
+                data={result.release_date}
+                resume={result.overview}
+              />
+            ))}
     </div>
   );
 }
